f943cd5beea001d1881c88f3e99e3c6e935fbe07
choco294 <abural.729@gmail.com>
2023-06-05 17:14:59 +0900

    v1.1

--- SearchWithBing.bat
+++ SearchWithBing.bat
@@ -15,7 +15,7 @@ FOR %%a IN (%WORD_PC%) DO (
 
 SET /A CNT_PC-=1
 FOR /L %%I in (0,1,%CNT_PC%) DO (
-  @REM TIMEOUT /NOBREAK 3 > NUL
+  TIMEOUT /NOBREAK 3 > NUL
   @REM ECHO %!%WORD_PC[%%I]%!%=!WORD_PC[%%I]!
   ECHO [%%I]!WORD_PC[%%I]!
   START "" msedge.exe https://www.bing.com/search?q=!WORD_PC[%%I]!
@@ -24,8 +24,6 @@ FOR /L %%I in (0,1,%CNT_PC%) DO (
 ECHO ****************************************************
 ECHO * END : Search with Bing by PC
 ECHO ****************************************************
-
-TIMEOUT /NOBREAK 15 > NUL
 
 ECHO ****************************************************
 ECHO * START : Search with Bing by Mobile
@@ -37,7 +35,7 @@ TIMEOUT /NOBREAK 3 > NUL
 
 SET WORD_MOBILE=delighted department designate develop donate acquisition admit eliminate encourage enroll estate exceed except executive exhibit expect expertise agenda allocate explain facility fail familiar feature
 SET CNT_MOBILE=0
-START "" msedge.exe --new-window --user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 EdgiOS/113.1774.50 Mobile/15E148 Safari/605.1.15"
+START "" msedge.exe --new-window --user-agent="Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36"
 
 FOR %%a IN (%WORD_MOBILE%) DO (
   SET %!%WORD_MOBILE[!CNT_MOBILE!]%!%=%%a
@@ -46,7 +44,7 @@ FOR %%a IN (%WORD_MOBILE%) DO (
 
 SET /A CNT_MOBILE-=1
 FOR /L %%I in (0,1,%CNT_MOBILE%) DO (
-  @REM TIMEOUT /NOBREAK 3 > NUL
+  TIMEOUT /NOBREAK 3 > NUL
   @REM ECHO %!%WORD_MOBILE[%%I]%!%=!WORD_MOBILE[%%I]!
   ECHO [%%I]!WORD_MOBILE[%%I]!
   START "" msedge.exe https://www.bing.com/search?q=!WORD_MOBILE[%%I]!
@@ -56,9 +54,9 @@ ECHO ****************************************************
 ECHO * END : Search with Bing by Mobile
 ECHO ****************************************************
 
-TIMEOUT /NOBREAK 15 > NUL
+TIMEOUT /NOBREAK 3 > NUL
 TASKKILL /IM msedge.exe /F > NUL 2>&1
-START "" msedge.exe --no-startup-window --win-session-start /prefetch:5
+@REM START "" msedge.exe --no-startup-window --win-session-start /prefetch:5
 START "" msedge.exe --window-size=1000,600
 
 ECHO Press the ENTER Key...
